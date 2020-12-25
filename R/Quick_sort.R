c1007fbc4d89c1ba5a2ef105a2b27079aa77a397
unknown <bjawad@bbox.fr>
2020-12-25 17:55:27 +0100

    3 way partition à faire

--- R/Quick_sort.R
+++ R/Quick_sort.R
@@ -63,6 +63,71 @@ create_hoare_partition <- function(V)
   }
 }
 
+create_three_way_partition <- function(V)
+{
+  
+  V_res <- V
+  
+  # Choix du pivot (choisi aléatoirement parmi les éléments du vecteur à trier)
+  pivot <- sample(V_res, 1)
+  
+  #pivot <- V_res[pivot_index]
+  
+  # i concerne la partie gauche du vecteur (à gauche du pivot)
+  i <- 1
+  # j concerne la partie droite du vecteur (à droite du pivot)
+  j <- length(V_res)
+  
+  # On répète la procédure tant qu'on a pas placé tout les éléments strictement inférieurs au pivot
+  # à sa gauche et les éléments strictement supérieurs au pivot à sa droite
+  while(TRUE)
+  {
+    # Si un élément est supérieur ou égal au pivot on s'arrête (c'est un élément mal placé)
+    while(V_res[i] < pivot)
+    {
+      # Tant que les éléments qu'on regarde en partant du début du vecteur
+      # sont plus petits que le pivot, on incrémente l'indice
+      # qui permet de compter les éléments à gauche
+      i <- i + 1
+    }
+    # Tant que les éléments qu'on regarde en partant de la fin du vecteur
+    # sont plus grands que le pivot, on décrémente l'indice
+    # qui permet de compter les éléments à droite
+    # Si un élément est inférieur ou égal au pivot, on s'arrête (c'est un élément mal placé)
+    while(V_res[j] > pivot)
+    {
+      j <- j - 1
+    }
+    
+    # Si i >= j, on renvoie j et le vecteur modifié
+    
+    if(i >= j)
+    {
+      return(list(new_pivot_index = j, new_V = V_res))
+    }
+    
+    # Si i < j, on échange V[i] et V[j] puis on recommence la procédure
+    # Il faut les échanger car si on s'est arrêté avant "le milieu" pour i ou pour j
+    # ça veut dire que dans le(s) cas en question, l'élément où on s'est arrêté
+    # était mal placé.
+    tmp_i <- V_res[i]
+    V_res[i] <- V_res[j]
+    V_res[j] <- tmp_i
+    
+    # Si i et j pointent sur le même élément, on tombe sur une boucle infinie
+    # on force donc j à baisser de 1 pour que l'algorithme continue
+    # dès que j = i, l'algorithme se stoppera
+    if(V_res[i] == V_res[j])
+    {
+      j <- j - 1
+    }
+  }
+}
+
+
+
+
+
 
 quick_sort <- function(V)
 {
