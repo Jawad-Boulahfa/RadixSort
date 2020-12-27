a1f5e10d42d3fb0128a3e684d06be368316a5558
unknown <bjawad@bbox.fr>
2020-12-27 15:48:29 +0100

    Updated

--- R/Quick_sort.R
+++ R/Quick_sort.R
@@ -63,67 +63,7 @@ create_hoare_partition <- function(V)
   }
 }
 
-create_three_way_partition <- function(V)
-{
-  
-  V_res <- V
-  
-  # Choix du pivot (choisi aléatoirement parmi les éléments du vecteur à trier)
-  pivot <- sample(V_res, 1)
-  
-  #pivot <- V_res[pivot_index]
-  
-  # i concerne la partie gauche du vecteur (à gauche du pivot)
-  i <- 1
-  # j concerne la partie droite du vecteur (à droite du pivot)
-  j <- length(V_res)
-  
-  # On répète la procédure tant qu'on a pas placé tout les éléments strictement inférieurs au pivot
-  # à sa gauche et les éléments strictement supérieurs au pivot à sa droite
-  while(TRUE)
-  {
-    # Si un élément est supérieur ou égal au pivot on s'arrête (c'est un élément mal placé)
-    while(V_res[i] < pivot)
-    {
-      # Tant que les éléments qu'on regarde en partant du début du vecteur
-      # sont plus petits que le pivot, on incrémente l'indice
-      # qui permet de compter les éléments à gauche
-      i <- i + 1
-    }
-    # Tant que les éléments qu'on regarde en partant de la fin du vecteur
-    # sont plus grands que le pivot, on décrémente l'indice
-    # qui permet de compter les éléments à droite
-    # Si un élément est inférieur ou égal au pivot, on s'arrête (c'est un élément mal placé)
-    while(V_res[j] > pivot)
-    {
-      j <- j - 1
-    }
-    
-    # Si i >= j, on renvoie j et le vecteur modifié
-    
-    if(i >= j)
-    {
-      return(list(new_pivot_index = j, new_V = V_res))
-    }
-    
-    # Si i < j, on échange V[i] et V[j] puis on recommence la procédure
-    # Il faut les échanger car si on s'est arrêté avant "le milieu" pour i ou pour j
-    # ça veut dire que dans le(s) cas en question, l'élément où on s'est arrêté
-    # était mal placé.
-    tmp_i <- V_res[i]
-    V_res[i] <- V_res[j]
-    V_res[j] <- tmp_i
-    
-    # Si i et j pointent sur le même élément, on tombe sur une boucle infinie
-    # on force donc j à baisser de 1 pour que l'algorithme continue
-    # dès que j = i, l'algorithme se stoppera
-    if(V_res[i] == V_res[j])
-    {
-      j <- j - 1
-    }
-  }
-}
-
+# Quick sort avec la méthode hoare (pas optimal, utiliser le three way partitioning est mieux)
 quick_sort <- function(V)
 {
   if(length(V) > 1)
@@ -166,16 +106,20 @@ quick_sort <- function(V)
   }
 }
 
+# Quick sort avec three way partitioning
 quick_sort_opti <- function(V)
 { 
   if(length(V) > 1)
   {
-    tri=match(TRUE,diff(V)<0) #check et indique le premier emplacement tel que V[i]>V[i+1] donc mal ordonné.
-    if(is.na(tri)) {return(V)} #Si les éléments sont bien classés alors on renvoie le vecteur directement.
+    # Check et indique le premier emplacement tel que V[i]>V[i+1] donc mal ordonné.
+    tri <- match(TRUE, diff(V) < 0)
+    # Si les éléments sont bien classés alors on renvoie le vecteur directement.
+    if(is.na(tri))
+      return(V)
     
-    pivot=sample(V,1);
+    pivot <- sample(V, 1)
     
-    V_left <- V[V<pivot]
+    V_left <- V[V < pivot]
     if(length(V_left) > 1)
       V_left <- quick_sort_opti(V = V_left)
     
@@ -183,7 +127,8 @@ quick_sort_opti <- function(V)
     if(length(V_right) > 1)
       V_right <- quick_sort_opti(V = V_right)
     
-    V_middle=V[V==pivot]
+    V_middle <- V[V == pivot]
+    
     res <- c(V_left,V_middle,V_right)
     return(res)
   }
